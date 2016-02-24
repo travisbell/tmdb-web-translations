0e990163186dc96e97de20b9b714d845728f352d
Travis Bell <travis@fanhattan.com>
2016-02-24 14:14:50 -0700

    Moving some translation logic into the I18n config object.

--- lib/tmdb_web_translations.rb
+++ lib/tmdb_web_translations.rb
@@ -1,9 +1,58 @@
 module TMDb
   module Config
     class I18n
+
+      def self.fallbacks
+        { 'en' => 'en-US',
+          'bg' => 'bg-BG',
+          'bn' => 'bn-BD',
+          'es' => 'es-ES',
+          'fi' => 'fi-FI',
+          'fr' => 'fr-FR',
+          'ko' => 'ko-KR',
+          'pt' => 'pt-PT',
+          'sv' => 'sv-SE',
+          'tr' => 'tr-TR',
+          'zh' => 'zh-CN' }
+      end
+
       def self.load_path
         File.dirname(__FILE__) + "/../locales"
       end
+
+      def self.supported_iso_639_1
+        [ 'en',
+          'bg',
+          'bn',
+          'cs',
+          'da',
+          'de',
+          'el',
+          'es',
+          'fi',
+          'fr',
+          'he',
+          'hu',
+          'id',
+          'it',
+          'ja',
+          'ka',
+          'ko',
+          'nl',
+          'no',
+          'pl',
+          'pt',
+          'ro',
+          'ru',
+          'sl',
+          'sr',
+          'sv',
+          'th',
+          'tr',
+          'uk',
+          'zh' ]
+      end
+
     end
   end
 end
