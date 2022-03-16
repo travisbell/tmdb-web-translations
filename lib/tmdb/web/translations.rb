c4bac1b10010801e7a6dd309e6b466a530f59edd
Travis Bell <me@travisbell.com>
2022-03-16 14:53:40 -0600

    Path update.

--- lib/tmdb/web/translations.rb
+++ lib/tmdb/web/translations.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require 'i18n'
-require 'shared/models'
+require "i18n"
+require "tmdb/shared"
 
-require_relative 'translations/i18n'
-require_relative 'translations/i18n_fallbacks'
-require_relative 'translations/version'
+require_relative "translations/i18n"
+require_relative "translations/i18n_fallbacks"
+require_relative "translations/version"
