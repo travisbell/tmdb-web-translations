45a16272e392d53ce5b4a8177c0af07aaa0babd8
Travis Bell <me@travisbell.com>
2024-02-08 10:33:18 -0700

    Match available locales to the data we get back from JustWatch.

--- lib/tmdb/web/translations/i18n.rb
+++ lib/tmdb/web/translations/i18n.rb
@@ -61,23 +61,27 @@ module TMDb
         'sq' => 'sq-AL',
         'sr' => 'sr-RS',
         'sv' => 'sv-SE',
+        'sw' => 'sw-TZ',
         'ta' => 'ta-IN',
         'te' => 'te-IN',
         'tl' => 'tl-PH',
         'th' => 'th-TH',
         'tr' => 'tr-TR',
         'uk' => 'uk-UA',
+        'ur' => 'ur-PK',
         'vi' => 'vi-VN',
         'zh' => 'zh-CN',
         'zu' => 'zu-ZA'
       }.freeze
 
       IGNORED_TRANSLATIONS = [
-        'en-AU',
-        'en-CA',
-        'en-GB',
-        'en-IE',
-        'en-NZ'
+        'ar-BH', 'ar-EG', 'ar-IQ', 'ar-JO', 'ar-LY', 'ar-MA', 'ar-QA', 'ar-YE', 'ar-TD', 'ca-AD',
+        'en-AG', 'el-CY', 'en-AG', 'en-AU', 'en-BB', 'en-BZ', 'en-CA', 'en-CM', 'en-GB', 'en-GG',
+        'en-GH', 'en-GI', 'en-GY', 'en-IE', 'en-JM', 'en-KE', 'en-LC', 'en-MW', 'en-NZ', 'en-PG',
+        'en-TC', 'en-ZM', 'en-ZW', 'es-AR', 'es-CL', 'es-DO', 'es-EC', 'es-GT', 'es-GQ', 'es-HN',
+        'es-NI', 'es-PA', 'es-PE', 'es-PY', 'es-SV', 'es-UY', 'fr-BF', 'fr-CD', 'fr-CI', 'fr-GF',
+        'fr-GP', 'fr-MC', 'fr-ML', 'fr-MU', 'fr-PF', 'it-VA', 'pt-AO', 'pt-MZ', 'ro-MD', 'sr-ME',
+        'sq-XK'
       ].freeze
 
       # This list is based on LANGUAGE_CODES, it contains all assigned ISO-369-1
@@ -98,20 +102,75 @@ module TMDb
       end
 
       def self.default_iso_3166_1_mapping
-        @default_iso_3166_1_mapping ||= TMDb::Web::Translations::DEFAULT_MAPPING.invert.merge!(
+        @default_iso_3166_1_mapping ||= TMDb::Web::Translations::DEFAULT_MAPPING.invert.merge(
           'ar-AE' => 'ar',
+          'ar-BH' => 'ar',
+          'ar-EG' => 'ar',
+          'ar-IQ' => 'ar',
+          'ar-JO' => 'ar',
+          'ar-LY' => 'ar',
+          'ar-MA' => 'ar',
+          'ar-QA' => 'ar',
+          'ar-YE' => 'ar',
+          'ar-TD' => 'ar',
+          'ca-AD' => 'ca',
           'de-AT' => 'de',
           'de-CH' => 'de',
+          'el-CY' => 'el',
+          'en-AG' => 'en',
           'en-AU' => 'en',
+          'en-BB' => 'en',
+          'en-BZ' => 'en',
           'en-CA' => 'en',
+          'en-CM' => 'en',
           'en-GB' => 'en',
+          'en-GG' => 'en',
+          'en-GH' => 'en',
+          'en-GI' => 'en',
+          'en-GY' => 'en',
           'en-IE' => 'en',
+          'en-JM' => 'en',
+          'en-KE' => 'en',
+          'en-LC' => 'en',
+          'en-MW' => 'en',
           'en-NZ' => 'en',
+          'en-PG' => 'en',
+          'en-TC' => 'en',
+          'en-ZM' => 'en',
+          'en-ZW' => 'en',
+          'es-AR' => 'es',
+          'es-CL' => 'es',
+          'es-DO' => 'es',
+          'es-EC' => 'es',
+          'es-GT' => 'es',
+          'es-GQ' => 'es',
+          'es-HN' => 'es',
           'es-MX' => 'es',
+          'es-NI' => 'es',
+          'es-PA' => 'es',
+          'es-PE' => 'es',
+          'es-PY' => 'es',
+          'es-SV' => 'es',
+          'es-UY' => 'es',
+          'fr-BF' => 'fr',
           'fr-CA' => 'fr',
+          'fr-CD' => 'fr',
+          'fr-CI' => 'fr',
+          'fr-GF' => 'fr',
+          'fr-GP' => 'fr',
+          'fr-MC' => 'fr',
+          'fr-ML' => 'fr',
+          'fr-MU' => 'fr',
+          'fr-PF' => 'fr',
+          'it-VA' => 'it',
           'ms-SG' => 'ms',
           'nl-BE' => 'nl',
+          'pt-AO' => 'pt',
           'pt-BR' => 'pt',
+          'pt-MZ' => 'pt',
+          'ro-MD' => 'ro',
+          'sr-ME' => 'sr',
+          'sq-XK' => 'sq',
           'zh-HK' => 'zh',
           'zh-SG' => 'zh',
           'zh-TW' => 'zh'
@@ -138,7 +197,7 @@ module TMDb
       def self.default_language_i18n
         @default_language_i18n ||= LANGUAGE_CODES.each_with_object({}) { |iso_639_1, hash|
           hash[iso_639_1] = "#{iso_639_1}-#{iso_639_1.upcase}"
-        }.merge!(TMDb::Web::Translations::DEFAULT_MAPPING).freeze
+        }.merge(TMDb::Web::Translations::DEFAULT_MAPPING).freeze
       end
 
       def self.default_language_to_country_mapping
@@ -148,7 +207,7 @@ module TMDb
       def self.language_list
         @language_list ||= (LANGUAGE_CODES - TMDb::Web::Translations.supported_iso_639_1).each_with_object({}) do |iso_639_1, hash|
           hash["#{iso_639_1}-#{iso_639_1.upcase}"] = "#{iso_639_1}"
-        end.merge!(TMDb::Web::Translations.default_iso_3166_1_mapping).sort_by { |h,v| v }.to_h.freeze
+        end.merge(TMDb::Web::Translations.default_iso_3166_1_mapping).sort_by { |h,v| v }.to_h.freeze
       end
 
       def self.language_path
