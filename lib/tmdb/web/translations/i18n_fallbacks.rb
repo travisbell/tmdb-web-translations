5946f2c94ee45ab7d4888198c5b83ffa32a41076
Travis Bell <me@travisbell.com>
2024-03-04 18:27:52 -0700

    Bumping translations.

--- lib/tmdb/web/translations/i18n_fallbacks.rb
+++ lib/tmdb/web/translations/i18n_fallbacks.rb
@@ -1,10 +1,9 @@
-# encoding: utf-8
 # frozen_string_literal: true
 
 require 'i18n/backend/fallbacks'
 
 # I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
-I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+I18n::Backend::Simple.include I18n::Backend::Fallbacks
 
 I18n.load_path += Dir[TMDb::Web::Translations.load_path + '/*.yml']
 I18n.load_path += Dir[TMDb::Web::Translations.country_path + '/*.yml']
@@ -13,12 +12,12 @@ I18n.load_path += Dir[TMDb::Web::Translations.transliteration_path + '/*.rb']
 I18n.load_path += Dir[TMDb::Web::Translations.transliteration_path + '/*.yml']
 
 # I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+
 I18n.available_locales = (
   TMDb::Web::Translations.default_iso_3166_1_mapping.keys +
   TMDb::Web::Translations.default_language_i18n.values +
   TMDb::Web::Translations.default_iso_3166_1_i18n.values
 ).uniq
-
 I18n.enforce_available_locales = true
 I18n.default_locale = 'en-US'
 
@@ -27,7 +26,7 @@ TMDb::Web::Translations.default_iso_3166_1_mapping.each do |k, v|
 end
 
 TMDb::Web::Translations.default_language_i18n.each do |k, v|
-  I18n.fallbacks.map(k => [v, "en-US"])
+  I18n.fallbacks.map(k => [v, 'en-US'])
 end
 
 TMDb::Web::Translations.default_iso_3166_1_i18n.each do |k, v|
@@ -41,6 +40,7 @@ module I18n
       class HashTransliterator
         def transliterate(string, replacement = nil)
           return '' unless string
+
           replacement ||= DEFAULT_REPLACEMENT_CHAR
           string.gsub(/[^\x00-\x7f]/u) do |char|
             approximations[char] || replacement
